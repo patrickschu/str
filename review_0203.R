dc747195c4ecd75664f04bdc888812eb1e66eece
patrickschu <patrickschu@users.noreply.github.com>
2016-02-03 11:46:55 -0600

    Create review_0203.R

---
+++ review_0203.R
@@ -0,0 +1,41 @@
+##erica and patrick look at STR data
+##it is the goodest of time
+
+str=read.csv("/Users/ps22344/Downloads/allspeakers_normalized_metadata_lemmas.csv", header=T)
+#summary(str)
+
+#setting up the wdir
+setwd("/Users/ps22344/Downloads/plots")
+
+
+#let us plot the s, sh and str for each speaker
+
+speakers=levels(str$speaker)
+print (speakers)
+
+for (spkr in speakers)
+{
+	print (spkr);
+	#this is the data for the speaker only
+	tempdata=str[str$speaker==spkr,];
+	# print ("SS")
+	#let us try a little thing that prints out how many tokens we have for each sound
+	# for (var in c("S", "SH", "STR")):
+	# {
+		# print (var);
+		# print (nrow(tempdata[tempdata$env==var,]))
+	# }
+	# print ("SH");
+	# print (summary(tempdata[tempdata$env=="SH",]));
+	# print ("STR");
+	# print (summary(tempdata[tempdata$env=="STR",]));
+	# print ("\n\n\n------------\n\n");
+	png(paste(spkr, ".png"), width=550, height=550);
+	#just setting up the plot here
+	plot(tempdata[tempdata$env=="S",]$cog2, type="n", ylim=c(234,11626), xlim=c(0,7), main=spkr);
+	#here be the plots
+	points(tempdata[tempdata$env=="STR",]$env, tempdata[tempdata$env=="STR",]$cog2);
+	points(tempdata[tempdata$env=="S",]$env,tempdata[tempdata$env=="S",]$cog2, pch="S");
+	points(tempdata[tempdata$env=="SH",]$env, tempdata[tempdata$env=="SH",]$cog2, pch="ʃ");
+	dev.off()
+}
